91271d59bc2eb9c272e1b3e68f268f6d267dfe80
Nathan Lilienthal <nathan@nixpulvis.com>
2014-04-28 21:13:57 -0400

    Move spec only extensions into spec/

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,8 @@ require 'ostruct'
 require 'json_array_serializer'
 
 # Connivance extensions.
-require_relative '../ext/hash'
-require_relative '../ext/open_struct'
+require_relative 'ext/hash'
+require_relative 'ext/open_struct'
 
 RSpec.configure do |config|
   config.color_enabled = true
